3de8950061d1b80297687e8433ec80a1f95a2170
thirdsanctuary <sanctuary.third@gmail.com>
2024-10-22 13:54:10 -0400

    update to 1.21.2

--- src/main/java/dev/jadeposting/gatekeep/mixins/ClientPacketListenerMixin_RemoveToast.java
+++ src/main/java/dev/jadeposting/gatekeep/mixins/ClientPacketListenerMixin_RemoveToast.java
@@ -2,7 +2,7 @@ package dev.jadeposting.gatekeep.mixins;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.client.gui.components.toasts.Toast;
-import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.components.toasts.ToastManager;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,10 +13,10 @@ public class ClientPacketListenerMixin_RemoveToast {
         method = "handleLogin",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/client/gui/components/toasts/ToastComponent;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"
+            target = "Lnet/minecraft/client/gui/components/toasts/ToastManager;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"
         )
     )
-    private boolean removeToast(ToastComponent instance, Toast toast) {
+    private boolean removeToast(ToastManager instance, Toast toast) {
         return false;
     }
 }
